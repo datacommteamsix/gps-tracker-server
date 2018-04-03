8db87e33e4341b9088ce5f6279a07cd56f98c28c
angusl2004 <angusl2004@gmail.com>
2018-04-03 10:42:38 -0700

    Fixed the way certain exceptions are handled so that the server doesn't crash

--- src/main/java/Asn3/ServerThread.java
+++ src/main/java/Asn3/ServerThread.java
@@ -9,28 +9,46 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This is the servant thread that is spawned by the
+ * server to handle each client that connects
+ *
+ * @author Angus Lam
+ * @version 4-3-2018
+ */
 public class ServerThread extends Thread {
+    /**
+     * The client socket
+     */
     private Socket clientSocket;
+    /**
+     * A reference to some part of the Firebase database
+     */
     private DatabaseReference databaseReference;
 
     private static final int HASHED_DEVICE_ID_INDEX = 0;
     private static final int TIMESTAMP_INDEX = 1;
     private static final int LATITUDE_INDEX = 2;
     private static final int LONGITUDE_INDEX = 3;
+    private static final int TIMEOUT = 20000;
 
     /**
-     * The Server Constructor
+     * This creates a thread with the client socket
+     * and a reference to the firebase database
      *
+     * @param clientSocket
+     * @param databaseReference
      * @throws IOException
      */
     public ServerThread(Socket clientSocket, DatabaseReference databaseReference) throws IOException {
         this.clientSocket      = clientSocket;
         this.databaseReference = databaseReference;
-        clientSocket.setSoTimeout(20000); // set a 20 second timeout
+        clientSocket.setSoTimeout(TIMEOUT); // set a 20 second timeout
     }
 
     /**
@@ -41,74 +59,77 @@ public class ServerThread extends Thread {
                 // This will receive the responses from the client
                 DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 
-
                 while(true) {
+                    try {
+                        if(in.available() > 0) {
+                            // Count the number of available bytes in the stream
+                            byte[] buffer = new byte[59];
+                            System.out.println("Available bytes to read from stream : " + in.available() + " | buffer length: " + buffer.length);
+                            // Create a buffer the size of the available bytes in stream
+                            in.readFully(buffer);
 
-                    if(in.available() > 0) {
-                        // Count the number of available bytes in the stream
-                        byte[] buffer = new byte[59];
-                        System.out.println("Available bytes to read from stream : " + in.available() + " | buffer length: " + buffer.length);
-                        // Create a buffer the size of the available bytes in stream
-                        in.readFully(buffer);
-//                    buffer = new byte[count];
+                            List<byte[]> tokens = new ArrayList<byte[]>();
+                            final byte delimiter = ' ';
+                            int lastIndexOfDelimiter = -1;
 
-                        List<byte[]> tokens = new ArrayList<byte[]>();
-                        final byte delimiter = ' ';
-                        int lastIndexOfDelimiter = -1;
+                            //Break up the packet into tokens
+                            for (int i = 0; i < buffer.length; i++) {
+                                // If you hit a delimiter...
+                                if (buffer[i] == delimiter) {
+                                    int length = i - lastIndexOfDelimiter - 1;
+                                    byte[] token = new byte[length];
 
-                        //Break up the packet into tokens
-                        for (int i = 0; i < buffer.length; i++) {
-                            // If you hit a delimiter...
-                            if (buffer[i] == delimiter) {
-                                int length = i - lastIndexOfDelimiter - 1;
-                                byte[] token = new byte[length];
+                                    //Copy the token to array
+                                    System.arraycopy(buffer, lastIndexOfDelimiter + 1, token, 0, length);
+                                    //Add to the list
+                                    tokens.add(token);
 
-                                //Copy the token to array
-                                System.arraycopy(buffer, lastIndexOfDelimiter + 1, token, 0, length);
-                                //Add to the list
-                                tokens.add(token);
+                                    //Set the index
+                                    lastIndexOfDelimiter = i;
+                                }
 
-                                //Set the index
-                                lastIndexOfDelimiter = i;
+                                if (i == buffer.length - 1) {
+                                    int length = buffer.length - lastIndexOfDelimiter - 1;
+                                    byte[] token = new byte[length];
+
+                                    //Copy the token to array
+                                    System.arraycopy(buffer, lastIndexOfDelimiter + 1, token, 0, length);
+                                    //Add to the list
+                                    tokens.add(token);
+                                }
                             }
 
-                            if (i == buffer.length - 1) {
-                                int length = buffer.length - lastIndexOfDelimiter - 1;
-                                byte[] token = new byte[length];
+                            ByteBuffer wrapped;
 
-                                //Copy the token to array
-                                System.arraycopy(buffer, lastIndexOfDelimiter + 1, token, 0, length);
-                                //Add to the list
-                                tokens.add(token);
-                            }
+                            //Parse and print the content of the tokens
+                            System.out.print("The parsed packet: ");
+
+                            String hashedDeviceID = new String(tokens.get(HASHED_DEVICE_ID_INDEX));
+                            System.out.print(hashedDeviceID);
+                            System.out.print(" ");
+                            wrapped = ByteBuffer.wrap(tokens.get(TIMESTAMP_INDEX)); // big-endian by default
+                            long timestamp = wrapped.getLong();
+                            System.out.print(timestamp + " ");
+
+                            wrapped = ByteBuffer.wrap(tokens.get(LATITUDE_INDEX)); // big-endian by default
+                            double latitude = wrapped.getDouble();
+                            System.out.print(latitude + " ");
+
+                            wrapped = ByteBuffer.wrap(tokens.get(LONGITUDE_INDEX)); // big-endian by default
+                            double longitude = wrapped.getDouble();
+                            System.out.print(longitude + " \n");
+
+                            //Send the data to firebase
+                            System.out.println("Sending parsed data to Firebase");
+                            DatabaseReference device = databaseReference.child(hashedDeviceID);
+                            DatabaseReference time = device.child(Long.toString(timestamp));
+                            System.out.println(time.toString());
+                            time.setValueAsync(new GPSData(latitude, longitude));
                         }
-
-                        ByteBuffer wrapped;
-
-                        //Parse and print the content of the tokens
-                        System.out.print("The parsed packet: ");
-
-                        String hashedDeviceID = new String(tokens.get(HASHED_DEVICE_ID_INDEX));
-                        System.out.print(hashedDeviceID);
-                        System.out.print(" ");
-                        wrapped = ByteBuffer.wrap(tokens.get(TIMESTAMP_INDEX)); // big-endian by default
-                        long timestamp = wrapped.getLong();
-                        System.out.print(timestamp + " ");
-
-                        wrapped = ByteBuffer.wrap(tokens.get(LATITUDE_INDEX)); // big-endian by default
-                        double latitude = wrapped.getDouble();
-                        System.out.print(latitude + " ");
-
-                        wrapped = ByteBuffer.wrap(tokens.get(LONGITUDE_INDEX)); // big-endian by default
-                        double longitude = wrapped.getDouble();
-                        System.out.print(longitude + " \n");
-
-                        //Send the data to firebase
-                        System.out.println("Sending parsed data to Firebase");
-                        DatabaseReference device = databaseReference.child(hashedDeviceID);
-                        DatabaseReference time = device.child(Long.toString(timestamp));
-                        System.out.println(time.toString());
-                        time.setValueAsync(new GPSData(latitude, longitude));
+                    } catch (BufferUnderflowException e){
+                        e.printStackTrace();
+                    } catch (IndexOutOfBoundsException e){
+                        e.printStackTrace();
                     }
                 }
 
@@ -120,13 +141,6 @@ public class ServerThread extends Thread {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-    }
-
-    private String generatePolylineEncoding(double latitude, double longitude) {
-        LatLng point = new LatLng(latitude, longitude);
-        List<LatLng> coords = new ArrayList<LatLng>();
-        coords.add(point);
-        return PolylineEncoding.encode(coords);
     }
 }
 
